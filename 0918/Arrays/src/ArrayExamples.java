22193312ac45afea61609ab5fe857b34cefbabdc
Joshua Crotts <joshuacrotts@outlook.com>
2023-10-04 12:57:32 -0400

    Removed 1002

--- 0918/Arrays/src/ArrayExamples.java
+++ 0918/Arrays/src/ArrayExamples.java
@@ -1,5 +1,71 @@
 class ArrayExamples {
-    public static void main(String[] args) {
-        
+
+    static double average(int[] array) {
+        return 0;
     }
+
+    /**
+     * Computes the longest "run" of a set of numbers. The longest run is the longest
+     * contiguous sequence of some number.
+     * @param chars
+     * @return an integer array (tuple) containing two values: index 0 is the number, index 1 is the count/run length.
+     */
+    static int[] longestRun(int[] chars) {
+        int longest = chars[0];
+        int maxRun = 1;
+        int currentRun = 1;
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] == chars[i - 1]) {
+                currentRun++;
+            } else {
+                currentRun = 1;
+            }
+
+            // If we just exceeded the current longest:
+            if (currentRun > maxRun) {
+                longest = chars[i];
+                maxRun = currentRun;
+            }
+        }
+        // TODO.
+        return new int[]{longest, maxRun};
+    }
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+            int longest = chars[0];
+        int maxRun = 1;
+        int currentRun = 1;
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] == chars[i - 1]) {
+                currentRun++;
+            } else {
+                currentRun = 1;
+            }
+
+            // If we just exceeded the current longest:
+            if (currentRun > maxRun) {
+                longest = chars[i];
+                maxRun = currentRun;
+            }
+        }
+        // TODO.
+        return new int[]{longest, maxRun};
+     */
+
+
+
 }
--- 0918/Arrays/src/ArrayExamplesTest.java
+++ 0918/Arrays/src/ArrayExamplesTest.java
@@ -1,11 +1,26 @@
+import java.lang.reflect.Array;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ArrayExamplesTest {
 
     @org.junit.jupiter.api.Test
     void average() {
+        assertAll(
+                () -> assertEquals(70.4, ArrayExamples.average(new int[]{90, 80, 105, 50, 25}), 0.01),
+                () -> assertEquals(0, ArrayExamples.average(new int[]{}), 0.01),
+                () -> assertEquals(-55555.0, ArrayExamples.average(new int[]{-55555}), 0.01)
+        );
     }
 
     @org.junit.jupiter.api.Test
     void longestRun() {
+        assertAll(
+                () -> assertArrayEquals(new int[]{6, 4}, ArrayExamples.longestRun(new int[]{9, 5, 4, 4, 4, 3, 3, 2, 6, 6, 6, 6, 11, 12, 11, 11, 11, 21, 12, 11})),
+                () -> assertArrayEquals(new int[]{12, 12}, ArrayExamples.longestRun(new int[]{12,12,12,12,12,12,12,12,12,12,12,12})),
+                () -> assertArrayEquals(new int[]{212, 1}, ArrayExamples.longestRun(new int[]{212, 343, 211, 200, 101, 436, 438, 311})),
+                () -> assertArrayEquals(new int[]{211, 2}, ArrayExamples.longestRun(new int[]{212, 343, 211, 211, 101, 212, 212, 311}))
+
+        );
     }
 }
